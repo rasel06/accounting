f5b56774dd5634b919ab84d4fcd30face977a07c
rasel06 <rasel06@gmail.com>
2024-06-25 17:43:41 +0600

    Sidebar active issue solved

--- resources/views/components/helpers/parts/nav-item.blade.php
+++ resources/views/components/helpers/parts/nav-item.blade.php
@@ -10,12 +10,12 @@
 
 @if ($subMenu)
     <li>
-        <a href="/{{ $link }}"
+        <a wire:navigate href="/{{ $link }}"
             class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 group hover:bg-slate-300 dark:text-white dark:hover:bg-gray-700 pl-11 {{ $active }}">{{ $label }}</a>
     </li>
 @else
     <li>
-        <a href="/{{ $link }}"
+        <a wire:navigate href="/{{ $link }}"
             class="flex items-center p-2 text-base font-normal dark:text-white hover:bg-slate-300 dark:hover:bg-gray-700 {{ $active }} ">
             @if ($icon != '')
                 <span class="material-symbols-outlined">
