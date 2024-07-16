265e605fc601844ceab3ac4ec2675d65dbda818f
rasel06 <rasel06@gmail.com>
2024-07-16 16:22:32 +0600

    Almost done :)

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -1,5 +1,33 @@
 <div>
 
+
+
+    <div x-data="{ selectedTab: 'tab1' }" class="container mx-auto m-4">
+        <ul class="flex flex-wrap gap-4 border-b border-gray-200 bg-slate-200 p-4 rounded-md">
+            <li class="flex-1 text-center border-b-2 hover:border-blue-500"
+                :class="{ 'border-blue-500': selectedTab === 'tab1' }">
+                <a href="#" @click.prevent="selectedTab = 'tab1'">Information</a>
+            </li>
+            <li class="flex-1 text-center border-b-2 hover:border-blue-500"
+                :class="{ 'border-blue-500': selectedTab === 'tab2' }">
+                <a href="#" @click.prevent="selectedTab = 'tab2'">Password Change</a>
+            </li>
+            <li class="flex-1 text-center border-b-2 hover:border-blue-500"
+                :class="{ 'border-blue-500': selectedTab === 'tab3' }">
+                <a class="bg-rose-500 leading-6" href="#" @click.prevent="selectedTab = 'tab3'">Delete
+                    Account</a>
+            </li>
+
+        </ul>
+        <div class="mt-4 bg-slate-300 p-6 rounded-md shadow-md ">
+            <div x-show="selectedTab === 'tab1'">Content for Tab 1</div>
+            <div x-show="selectedTab === 'tab2'">Content for Tab 2</div>
+            <div x-show="selectedTab === 'tab3'">Content for Tab 3</div>
+        </div>
+    </div>
+
+
+
     <div>
         <div class="grid w-full  bg-slate-300">
             {{-- <ul class="flex flex-row gap-2 w-full  bg-slate-600 justify-between px-4 text-white">
